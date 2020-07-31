6a87d08b641a0b2d58d79136fa2b7eedffa0b6bd
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-31 23:20:09 +0300

    ft:configuring_port_and_global_env:backend

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const dotenv = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
 
+dotenv.config({path:'./config/config.env'})
 
 const app = express()
 
 app.get('/', (req,res) => res.send('Hello'))
+const PORT = process.env.PORT || 5000
 
-app.listen()
+app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
 
