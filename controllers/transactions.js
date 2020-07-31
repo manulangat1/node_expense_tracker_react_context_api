2dc0ba9208dbf3f8f0a7483aedfbcf8f2c354e53
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-01 00:34:55 +0300

    ft:add_fetch_transactions_controller:backend

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,23 +1,35 @@
 //all the methods that intercat with db 
-
+const Transaction = require('../models/Transaction')
 
 // @desc GET all Transaction
 // @route GET /api/v1/transaction 
 // @access Public
-exports.getTransactions = (req,res,next) => {
-    res.send('GET Transaction')
+exports.getTransactions = async (req,res,next) => {
+    try{
+        const transactions = await Transaction.find()
+        return res.status(200).json({
+            success:true,
+            count:transactions.length,
+            data:transactions
+        })
+    } catch (err){
+        return res.send(500).json({
+            success:false,
+            error:"Server Error"
+        })
+    }
 }
 
 // @desc GET add Transaction
 // @route POST /api/v1/transaction 
 // @access Public
-exports.addTransaction = (req,res,next) => {
+exports.addTransaction = async (req,res,next) => {
     res.send('POST Transaction')
 }
 
 // @desc Delete  Transaction
 // @route Delete /api/v1/transaction/:id
 // @access Public
-exports.deleteTransactions = (req,res,next) => {
+exports.deleteTransactions = async (req,res,next) => {
     res.send('DELETE Transaction')
 }
