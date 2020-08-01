25a98bdbea05ef8a6132f717969041895a889061
manulangat <emmanuelthedeveloper@gmail.com>
2020-08-01 16:29:26 +0300

    ft:static_file_config:backend

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
+const path = require('path')
 
 //connect DB 
 const connectDB = require('./config/db')
@@ -15,10 +16,18 @@ app.use(express.json())
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
+
 //connecting to the db 
 connectDB()
 app.use('/api/v1/transactions/',transactions)
 // app.get('/', (req,res) => res.send('Hello'))
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')))
+}
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
